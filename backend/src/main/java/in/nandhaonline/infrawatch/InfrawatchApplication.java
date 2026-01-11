df7b3689d6e24490d040fdfc680d98c27c09a1fa
Nandha Visvanathan <nandhavisvanathan25@gmail.com>
2026-01-11 16:46:15 +0530

    boiler plate set up done

---
+++ backend/src/main/java/in/nandhaonline/infrawatch/InfrawatchApplication.java
@@ -0,0 +1,13 @@
+package in.nandhaonline.infrawatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrawatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrawatchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/nandhaonline/infrawatch/InfrawatchApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nandhaonline.infrawatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrawatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
